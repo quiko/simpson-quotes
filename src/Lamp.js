f0068dda01aa347d6bc86dd6818de8510f95dfc5
quiko <ouladch@gmail.com>
2018-11-06 17:39:07 +0100

    miniature d'une react app

---
+++ src/Lamp.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Lamp extends Component {
+  
+  
+  constructor(props) 
+  {
+      super(props);
+      this.state = {
+        on: false
+      };
+    }
+    
+    handleClick = () => {
+      this.setState({ on: !this.state.on });
+    };
+  render() {
+    const light = this.props.on ? 'on' : 'off';
+    return (
+      <div className="Lamp">
+        <button className={light}>{light.toUpperCase()}</button>
+        <figure className={light} />
+      </div>
+    );
+  }
+}
+
+export default Lamp;
